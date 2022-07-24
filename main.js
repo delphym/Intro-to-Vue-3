4074acc28074a34737ef3175355e188830835ef5
DelphyM <5643604+delphym@users.noreply.github.com>
2022-07-25 00:36:17 +1200

    Mounted vue app and provided product and its description

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+// const product = 'Socks'
+const app = Vue.createApp({
+    data: function () {
+        return {
+            product: 'Lamp',
+            description: 'Extremely bright light'
+        }
+    }
+})
+
+
